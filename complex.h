d6b6446f9850a702a231feac9e8ae1edb4d847fb
Klarice <klaricevulpes@gmail.com>
2020-07-15 18:14:10 -0400

    works with doubles now

--- complex.h
+++ complex.h
@@ -25,16 +25,31 @@ class Complex {
             this->r = c.r;
             this->i = c.i;
         }
+        Complex(const double& n) {
+            this->r = n;
+            this->i = 0;
+        }
         Complex& operator=(const Complex& c) {
             this->r = c.r;
             this->i = c.i;
             return *this;
         }
+        Complex& operator=(const double& n) {
+            this->r = n;
+            this->i = 0;
+            return *this;
+        }
         Complex operator+(const Complex& c) {
             return Complex(this->r+c.r, this->i+c.i);
         }
+        Complex operator+(const double& n) {
+            return Complex(this->r+n, this->i);
+        }
         Complex operator-(const Complex& c) {
             return Complex(this->r-c.r, this->i-c.i);
+        }
+        Complex operator-(const double& n) {
+            return Complex(this->r-n, this->i);
         }
         Complex operator*(const Complex& c) {
             return Complex(
@@ -42,26 +57,48 @@ class Complex {
                 this->r*c.i+this->i*c.r
             );
         }
+        Complex operator*(const double& n) {
+            return Complex(this->r*n, this->i*n);
+        }
         Complex operator/(const Complex& c) {
             return Complex(
                 (this->r*c.r+this->i*c.i)/(c.r*c.r+c.i*c.i),
                 (-this->r*c.i+this->i*c.r)/(c.r*c.r+c.i*c.i)
             );
         }
+        Complex operator/(const double& n) {
+            return Complex(this->r/n, this->i/n);
+        }
         Complex& operator+=(const Complex& c) {
             *this = *this + c;
+            return *this;
+        }
+        Complex& operator+=(const double& n) {
+            *this = *this + n;
             return *this;
         }
         Complex& operator-=(const Complex& c) {
             *this = *this - c;
             return *this;
         }
+        Complex& operator-=(const double& n) {
+            *this = *this - n;
+            return *this;
+        }
         Complex& operator*=(const Complex& c) {
             *this = *this * c;
             return *this;
         }
+        Complex& operator*=(const double& n) {
+            *this = *this * n;
+            return *this;
+        }
         Complex& operator/=(const Complex& c) {
             *this = *this / c;
+            return *this;
+        }
+        Complex& operator/=(const double& n) {
+            *this = *this / n;
             return *this;
         }
         friend std::ostream& operator<<(std::ostream& os, const Complex& c) {
@@ -79,7 +116,7 @@ double abs(Complex c) {
     double s = c.real()*c.real()+c.imag()*c.imag();
     double a = s;
     while((a-s/a)>ROOT_EPSILON) {
-        a=(a+s/a)/2.0;
+        a=(a+s/a)/2.;
     }
     return a;
 }
